28911fdc59f4c056fb4eebae5922363805913726
Magdalena Piatkowska <magda.piatk@gmail.com>
2024-06-26 19:30:21 +0100

    second test failing!

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const cake = (recipe, ingredients) => {
-    number_of_cakes = 0
+    number_of_cakes = []
     for (item in recipe) {
         required_amount = recipe[item]
         available_amount = ingredients[item]
-        number_of_cakes = Math.floor(available_amount/required_amount)
+        number_of_cakes.push(Math.floor(available_amount/required_amount))
+        // number_of_cakes = Math.floor(available_amount/required_amount)
     }
-    return number_of_cakes
+    return Math.min(number_of_cakes)
 }
 
 module.exports = cake
--- app.test.js
+++ app.test.js
@@ -3,3 +3,8 @@ const cake = require('./app');
 test('Returns 2 as the amount of cakes that can be baked', () => {
     expect(cake({"flour": 100}, {"flour": 200})).toBe(2)
 });
+
+// This needs to be fixed!
+test('Returns 2 as the amount of cakes that can be baked', () => {
+    expect(cake({"flour": 100, "milk": 2}, {"flour": 200, "milk": 10})).toBe(2)
+});
