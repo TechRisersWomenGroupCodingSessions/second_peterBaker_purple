922a29062f8cb1c17b3fc5669dcdc6bf40921b5c
Magdalena Piatkowska <magda.piatk@gmail.com>
2024-06-26 19:26:12 +0100

    Start work on baker

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const cake = () => {
-    return 2;
+const cake = (recipe, ingredients) => {
+    number_of_cakes = 0
+    for (item in recipe) {
+        required_amount = recipe[item]
+        available_amount = ingredients[item]
+        number_of_cakes = Math.floor(available_amount/required_amount)
+    }
+    return number_of_cakes
 }
 
 module.exports = cake
--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const cake = require('./app');
 
 test('Returns 2 as the amount of cakes that can be baked', () => {
-    expect(cake()).toBe(2)
+    expect(cake({"flour": 100}, {"flour": 200})).toBe(2)
 });
