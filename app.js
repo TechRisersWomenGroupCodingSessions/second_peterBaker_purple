f46e89f73975f2f6cdf69f19871b6f5048905280
Magdalena Piatkowska <magda.piatk@gmail.com>
2024-06-26 19:32:05 +0100

    Making the TO-DO more visible

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cake = (recipe, ingredients) => {
         required_amount = recipe[item]
         available_amount = ingredients[item]
         number_of_cakes.push(Math.floor(available_amount/required_amount))
-        // number_of_cakes = Math.floor(available_amount/required_amount)
     }
     return Math.min(number_of_cakes)
 }
--- app.test.js
+++ app.test.js
@@ -4,7 +4,7 @@ test('Returns 2 as the amount of cakes that can be baked', () => {
     expect(cake({"flour": 100}, {"flour": 200})).toBe(2)
 });
 
-// This needs to be fixed!
+// TO-DO: make this work
 test('Returns 2 as the amount of cakes that can be baked', () => {
     expect(cake({"flour": 100, "milk": 2}, {"flour": 200, "milk": 10})).toBe(2)
 });
