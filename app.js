c8c66bde4737cbcf818403417032ceb8f4c2349a
GurTati <tatiana.gurova.j@gmail.com>
2024-07-10 18:47:21 +0100

    adding test for non numerical value

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cake = (recipe, ingredients) => {
     number_of_cakes = []
     for (item in recipe) {
         required_amount = recipe[item]
+        available_amount = 0;
         if (item in ingredients) {
             available_amount = ingredients[item]
         }
