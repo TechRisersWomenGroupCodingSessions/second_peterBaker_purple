7796f3191160d77e06251dd9183b55b8d8fce748
clarice_r <claricerdallorto@gmail.com>
2024-07-10 19:30:21 +0100

    Throw does not work

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const cake = (recipe, ingredients) => {
-    number_of_cakes = []
-    for (item in recipe) {
-        required_amount = recipe[item]
-        available_amount = 0;
-        if (item in ingredients) {
-            available_amount = ingredients[item]
-        }
-        number_of_cakes.push(Math.floor(available_amount/required_amount))
-    }
-    console.log(number_of_cakes)
-    min_number_of_cakes = Math.min(...number_of_cakes)
-    console.log(min_number_of_cakes)
-    console.log(Number.isNaN(min_number_of_cakes))
-    if (Number.isNaN(min_number_of_cakes)) {
-        console.log("I got here")
+    return
+    // number_of_cakes = []
+    // for (item in recipe) {
+    //     required_amount = recipe[item]
+    //     available_amount = 0;
+    //     if (item in ingredients) {
+    //         available_amount = ingredients[item]
+    //     }
+    //     number_of_cakes.push(Math.floor(available_amount/required_amount))
+    // }
+    // console.log(number_of_cakes)
+    // min_number_of_cakes = Math.min(...number_of_cakes)
+    // console.log(min_number_of_cakes)
+    // console.log(Number.isNaN(min_number_of_cakes))
+    // if (Number.isNaN(min_number_of_cakes)) {
+    //     console.log("I got here")
 
-        throw new TypeError();
+    //     throw new Error();
         
-    } else {
-        return min_number_of_cakes
-    }
-    
+    // } else {
+    //     return min_number_of_cakes
+    // }
 }
 
 module.exports = cake
