e10918fa9104c817590d7f73de7f249ed277a751
Magdalena Piatkowska <magda.piatk@gmail.com>
2024-07-10 18:35:21 +0100

    3rd test working but don't know why

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const cake = (recipe, ingredients) => {
     number_of_cakes = []
     for (item in recipe) {
         required_amount = recipe[item]
-        available_amount = ingredients[item]
+        if (item in ingredients) {
+            available_amount = ingredients[item]
+        }
+        console.log(item)
+        console.log(available_amount)
+        console.log(required_amount)
         number_of_cakes.push(Math.floor(available_amount/required_amount))
     }
-    return Math.min(number_of_cakes)
+    console.log(".....")
+    return Math.min(...number_of_cakes)
 }
 
 module.exports = cake
--- app.test.js
+++ app.test.js
@@ -4,7 +4,10 @@ test('Returns 2 as the amount of cakes that can be baked', () => {
     expect(cake({"flour": 100}, {"flour": 200})).toBe(2)
 });
 
-// TO-DO: make this work
 test('Returns 2 as the amount of cakes that can be baked', () => {
     expect(cake({"flour": 100, "milk": 2}, {"flour": 200, "milk": 10})).toBe(2)
+});
+
+test('Returns 0 as the amount of cakes that can be baked', () => {
+    expect(cake({"flour": 100, "milk": 2}, {"milk": 10})).toBe(0)
 });
