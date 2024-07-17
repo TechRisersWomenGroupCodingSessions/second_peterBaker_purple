1980aa22876300418684c37ed14b3c4e307b582c
clarice_r <claricerdallorto@gmail.com>
2024-07-17 18:19:01 +0100

    Testing raising exception

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const cake = (recipe, ingredients) => {
-    return
-    // number_of_cakes = []
-    // for (item in recipe) {
-    //     required_amount = recipe[item]
-    //     available_amount = 0;
-    //     if (item in ingredients) {
-    //         available_amount = ingredients[item]
-    //     }
-    //     number_of_cakes.push(Math.floor(available_amount/required_amount))
-    // }
-    // console.log(number_of_cakes)
-    // min_number_of_cakes = Math.min(...number_of_cakes)
-    // console.log(min_number_of_cakes)
-    // console.log(Number.isNaN(min_number_of_cakes))
-    // if (Number.isNaN(min_number_of_cakes)) {
-    //     console.log("I got here")
+    number_of_cakes = []
+    for (item in recipe) {
+        required_amount = recipe[item]
+        available_amount = 0;
+        if (item in ingredients) {
+            available_amount = ingredients[item]
+        }
+        number_of_cakes.push(Math.floor(available_amount/required_amount))
+    }
+    console.log(number_of_cakes)
+    min_number_of_cakes = Math.min(...number_of_cakes)
+    console.log(min_number_of_cakes)
+    console.log(isNaN(min_number_of_cakes))
+    if (isNaN(min_number_of_cakes)) {
+        console.log("I got here")
 
-    //     throw new Error();
+        throw new Error("custom error");
         
-    // } else {
-    //     return min_number_of_cakes
-    // }
+    } else {
+        return min_number_of_cakes
+    }
 }
 
 module.exports = cake
